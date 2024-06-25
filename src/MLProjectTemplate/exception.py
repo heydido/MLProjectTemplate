3c96341c0c04ea5fea0cb794be57532a5b48e4d7
heydido <aashish4.iitd@gmail.com>
2024-06-25 23:21:35 +0530

    logging the error

--- src/MLProjectTemplate/exception.py
+++ src/MLProjectTemplate/exception.py
@@ -18,6 +18,7 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        logging.error(self.error_message)
 
     def __str__(self):
         return self.error_message
